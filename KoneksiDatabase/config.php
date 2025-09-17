fdb4e0b45cac00bc5b30cffa7e5984a73559daf7
andiadbiyahkd <andiadbiyah16@gmail.com>
2025-09-17 23:58:54 +0800

    Change kode Connect Database

--- KoneksiDatabase/config.php
+++ KoneksiDatabase/config.php
@@ -1,14 +1,17 @@
 <?php
-$host = 'localhost';
-$db = 'JustBuyDB';
-$user = 'root';
-$pass = ''; // Ganti dengan password MySQL Anda jika ada
+$host = getenv('MYSQLHOST');
+$db   = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
 
 // Buat koneksi
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, (int)$port);
 
 // Cek koneksi
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Database connected successfully!";
 }
 ?>
